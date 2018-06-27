1ac9438d2160a06c39ddd7a0ce87528f95a374f3
crazysnail <bigqqpigljg@163.com>
2018-06-27 19:39:54 +0800

    GF study
    config componet

---
+++ Assets/GOS/Scripts/Configs/ConfigActor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameFramework.DataTable;
+using UnityEngine;
+public class ConfigHero : IDataRow
+{
+    public int Id { get; protected set; }
+    public string Name { get; private set; }
+    public int Hp { get; private set; }
+    public void ParseDataRow(string dataRowText)
+    {
+        string[] text = dataRowText.Split(',');
+        int index = 0;
+        index++; // 跳过#注释列
+        Id = int.Parse(text[index++]);
+        Name = text[index++];
+        Hp = int.Parse(text[index++]);
+    }
+}
--- Assets/GOS/Scripts/GF_Procedure/ProcedureLoad.cs
+++ Assets/GOS/Scripts/GF_Procedure/ProcedureLoad.cs
@@ -6,18 +6,32 @@ using UnityEngine;
 using UnityGameFramework.Runtime;
 using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
 using GameFramework.Event;
+using GameFramework.DataTable;
 
 
 namespace GameFrameworkGOS
 {
     public class ProcedureLoad : ProcedureBase
     {
-        private int mUIID = -1;
+        private const string mUIName = "Assets/GOS/Prefabs/UI/LoadUICanvas.prefab";
+        private const string mConfigName = "Assets/GOS/Configs/Hero.txt";
 
         protected override void OnEnter(ProcedureOwner procedureOwner)
         {
             base.OnEnter(procedureOwner);
 
+            //UI加载测速
+            UILoadTest();
+            // 加载表格
+            ConfigLoadTest();
+
+        }
+
+        /// <summary>
+        /// //////
+        /// </summary>
+        private void UILoadTest()
+        {
             // 加载框架Event组件
             EventComponent Event = UnityGameFramework.Runtime.GameEntry.GetComponent<EventComponent>();
 
@@ -25,15 +39,16 @@ namespace GameFrameworkGOS
             UIComponent UI = UnityGameFramework.Runtime.GameEntry.GetComponent<UIComponent>();
 
             // 订阅UI加载成功事件
-            Event.Subscribe(OpenUIFormSuccessEventArgs.EventId, OnOpenUIFormSuccess );
+            Event.Subscribe(OpenUIFormSuccessEventArgs.EventId, OnOpenUIFormSuccess);
+            Event.Subscribe(OpenUIFormFailureEventArgs.EventId, OnOpenUIFormFailure);
 
             // 加载UI
-            mUIID = UI.OpenUIForm("Assets/GOS/Prefabs/UI/LoadUICanvas.prefab", "DefaultGroup",this );
+            UI.OpenUIForm(mUIName, "DefaultGroup", this);
 
-            Log.Debug("UI load over sid=d%", mUIID);
+            //Event.Subscribe(OpenUIFormSuccessEventArgs.EventId, OnOpenUIFormSuccess);
+            //Event.Subscribe(OpenUIFormFailureEventArgs.EventId, OnOpenUIFormFailure);
+            UI.OpenUIForm("test"+mUIName, "DefaultGroup", this);
         }
-
-
 
         private void OnOpenUIFormSuccess(object sender, GameEventArgs e)
         {
@@ -47,7 +62,7 @@ namespace GameFrameworkGOS
                 if( UI != null ){
                     
                 }
-                UIForm form = UI.GetUIForm(mUIID);
+                UIForm form = UI.GetUIForm(mUIName);
                 if (form != null){
                     LoadUICanvas panel = (LoadUICanvas)(form.Logic);
                     if (panel != null)
@@ -59,6 +74,69 @@ namespace GameFrameworkGOS
 
             }
 
+        }
+        private void OnOpenUIFormFailure(object sender, GameEventArgs e)
+        {
+            Log.Error("OnOpenUIFormFailure :"+e.ToString());
+        }
+
+        /// <summary>
+        /// /////////////////
+        /// </summary>
+
+        private void ConfigLoadTest()
+        {
+            // 获取框架事件组件
+            EventComponent Event = UnityGameFramework.Runtime.GameEntry.GetComponent<EventComponent>();
+            // 获取框架数据表组件
+            DataTableComponent DataTable = UnityGameFramework.Runtime.GameEntry.GetComponent<DataTableComponent>();
+            // 订阅加载成功事件
+            Event.Subscribe(UnityGameFramework.Runtime.LoadDataTableSuccessEventArgs.EventId, OnLoadDataTableSuccess);
+            Event.Subscribe(UnityGameFramework.Runtime.LoadDataTableFailureEventArgs.EventId, OnLoadDataTableFailure);
+
+            // 加载配置表
+            DataTable.LoadDataTable<ConfigHero>("Hero", mConfigName,this);
+
+            // 订阅加载成功事件
+            //Event.Subscribe(UnityGameFramework.Runtime.LoadDataTableSuccessEventArgs.EventId, OnLoadDataTableSuccess);
+            //Event.Subscribe(UnityGameFramework.Runtime.LoadDataTableFailureEventArgs.EventId, OnLoadDataTableFailure);
+            DataTable.LoadDataTable<ConfigHero>("Hero", "ddddd", this);
+        }
+
+        private void OnLoadDataTableSuccess(object sender, GameEventArgs e)
+        {
+            // 获取框架数据表组件
+            DataTableComponent DataTable = UnityGameFramework.Runtime.GameEntry.GetComponent<DataTableComponent>();
+            // 获得数据表
+            IDataTable<ConfigHero> table = DataTable.GetDataTable<ConfigHero>();
+
+            // 获得所有行
+            ConfigHero[] rows = table.GetAllDataRows();
+            Log.Debug("ConfigHeros:" + rows.Length);
+
+            if (table != null)
+            {
+                // 根据行号获得某一行
+                ConfigHero row = table.GetDataRow(1); // 或直接使用 dtScene[1]
+                // 此行存在，可以获取内容了
+                string name = table.Name;
+                int hp = row.Hp;
+                Log.Debug("name:" + name + ", hp:" + hp);
+            }
+            else
+            {
+                // 此行不存在
+            }
+            // 获得满足条件的所有行
+            ConfigHero[] drScenesWithCondition = table.GetAllDataRows(x => x.Id > 0);
+
+            // 获得满足条件的第一行
+            ConfigHero drSceneWithCondition = table.GetDataRow(x => x.Name == "mutou");
+
+        }
+        private void OnLoadDataTableFailure(object sender, GameEventArgs e)
+        {
+            Log.Error("OnLoadDataTableFailure :" + e.ToString());
         }
     }
 }
