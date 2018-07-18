52a4c377debdefbc80833e1b9a83a2926c521d9c
lilong <348559311@qq.com>
2018-07-18 17:43:51 +0800

    个人、钱包页面ui

---
+++ Assets/GOS/Scripts/UI/MyWalletUICanvas.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using GameFramework;
+
+using UnityGameFramework.Runtime;
+using UnityEngine.UI;
+public class MyWalletUICanvas : UIFormLogic
+{
+    public Image ModifyInfo;
+    public Image OverviewBoundInfo;
+    public Image OverviewInfo;
+    public InputField InputSetCode;
+
+
+    // Use this for initialization
+    void Start () {
+        OverviewInfo.gameObject.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       // print(">>>>>>>>>>>>>>>>>>>>>>>>>zhege =====" + InputSetCode.text);
+    }
+
+    public void OnHelpClick()
+    {
+
+    }
+
+    public void OnOverviewClick()
+    {
+        ModifyInfo.gameObject.SetActive(false);
+        OverviewInfo.gameObject.SetActive(true);
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + ModifyInfo.flexibleWidth);
+    }
+    public void OnModifyClick()
+    {
+        ModifyInfo.gameObject.SetActive(true);
+        OverviewInfo.gameObject.SetActive(false);
+    }
+
+    public void OnImportClick()
+    {
+
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>zhege =====" + InputSetCode.text);
+        print(">>>>>>>>>>>>>>>>>------OnImportClick---------------------------");
+    }
+   
+}
